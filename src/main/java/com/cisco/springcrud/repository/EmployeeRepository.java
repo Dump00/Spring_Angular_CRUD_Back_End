5aa5ae516279d0174c95bd58459fd227c8a2d046
cisco <sovisrushain@gmail.com>
2021-12-29 21:43:59 +0530

    Implement EmployeeRepository interface

---
+++ src/main/java/com/cisco/springcrud/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.cisco.springcrud.repository;
+
+import com.cisco.springcrud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
